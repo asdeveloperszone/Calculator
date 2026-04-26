3d5b0c12ba2d22ab9ecbb4df4f0551738daeb5c2
AS DEVELOPER <asdeveloperszone@gmail.com>
2026-04-27 03:01:56 +0500

    Update MainActivity.kt

--- SimpleCalculator/app/src/main/java/com/asdroid/calculator/MainActivity.kt
+++ SimpleCalculator/app/src/main/java/com/asdroid/calculator/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        window.decorView.setBackgroundColor(android.Graphics.Color.BLACK)
         setContent {
             SimpleCalculatorTheme {
                 val viewModel = viewModel<CalculatorViewModel>()
