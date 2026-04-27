9322982b38667e610ae2c333d7021cbb8945d461
AS DEVELOPER <asdeveloperszone@gmail.com>
2026-04-27 08:06:00 +0500

    Update CalculatorScreen.kt

--- SimpleCalculator/app/src/main/java/com/asdroid/calculator/ui/CalculatorScreen.kt
+++ SimpleCalculator/app/src/main/java/com/asdroid/calculator/ui/CalculatorScreen.kt
@@ -36,8 +36,101 @@ fun CalculatorScreen(
       color = Color.White
     )
     // Inner Column creates here and Rows in it 
-
-
-    
+    Column(
+           modifier = Modifier
+           .fillMaxWidth()
+           .width(1f)
+           .padding(8.dp),
+      verticalArrangement = Arrangement.SpaceEvenly
+    ){
+      // Row 1
+    Row(
+      modifier =  Modifier.fillMaxWidth(),
+      horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+      CalculatorButton(symbol =  "C",backgroundColor = Color(0xFFFF5722), modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Clear)
+      }
+      CalculatorButton(symbol = "⌫",backgroundColor = Color(0xFFFF9800), modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Delete)
+      }
+      CalculatorButton(symbol = "÷", backgroundColor = Color(0xFF2196F3), modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Operation("÷"))
+      }
+      CalculatorButton(symbol = "×", backgroundColor = Color(0xFF2196F3), modifier = Modifier.weight(1f)){
+        onAction(CalculatorAction.Operation("×"))
+      }
+    }
+    // Row 2
+    Row(
+      modifier = Modifier.fillMaxWidth(),
+      horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+      CalculatorButton(symbol = "7", modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Number(7))
+      }
+      CalculatorButton(symbol = "8", modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Number(8))
+      }
+      CalculatorButton(symbol = "9", modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Number(9))
+      }
+      CalculatorButton(symbol = "-", backgroundColor = Color(0xFF2196F3), modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Operation("-"))
+      }
+  }
+  
+  // Row 3
+  Row(
+      modifier = Modifier.fillMaxWidth(),
+      horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+      CalculatorButton(symbol = "4", modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Number(4))
+      }
+      CalculatorButton(symbol = "5", modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Number(5))
+      }
+      CalculatorButton(symbol = "6", modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Number(6))
+      }
+      CalculatorButton(symbol = "+", backgroundColor = Color(0xFF2196F3),  modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Operation("+"))
+      }
+  }
+  
+  // Row 4
+  Row(
+      modifier = Modifier.fillMaxWidth(),
+      horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+      CalculatorButton(symbol = "1", modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Number(1))
+      }
+      CalculatorButton(symbol = "2", modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Number(2))
+      }
+      CalculatorButton(symbol = "3", modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Number(3))
+      }
+      CalculatorButton(symbol = ".", backgroundColor = Color(0xFF2196F3), modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Decimal)
+      }
+  }
+  
+  // Row 5 
+  
+  Row(
+      modifier = Modifier.fillMaxWidth(),
+      horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+      CalculatorButton(symbol = "0", modifier = Modifier.weight(2f)) {
+        onAction(CalculatorAction.Number(0))
+      }
+      CalculatorButton(symbol = "=", backgroundColor = Color(0xFF4CAF50),  modifier = Modifier.weight(1f)) {
+        onAction(CalculatorAction.Calculate)
+      }
+     }
+   }   
  }
 }
